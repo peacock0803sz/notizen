47b92972f429b40c09ea238a6613e700aece707d
Peacock <33555487+peacock0803sz@users.noreply.github.com>
2026-04-03 19:02:42 +0900

    :snowflake: Add pre-commit config in flake.nix

--- flake.nix
+++ flake.nix
@@ -2,26 +2,55 @@
   description = "notizen; a simple note-taking CLI";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import an internal flake module: ./other.nix
+        # To import an external flake module:
+        #   1. Add foo to inputs
+        #   2. Add foo as a parameter to the outputs function
+        #   3. Add here: foo.flakeModule
+
+        inputs.git-hooks.flakeModule
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        pre-commit.settings.hooks = {
+          golangci-lint.enable = true;
+          gofumpt = {
+            enable = true;
+            entry = "${pkgs.gofumpt}/bin/gofumpt -w";
+            types = [ "go" ];
+          };
         };
-      in
-      {
-        formatter = pkgs.nixpkgs-fmt;
-        flakedPkgs = pkgs;
+
+        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
         devShells.default = pkgs.mkShell {
+          inputsFrom = [ config.pre-commit.devShell ];
           packages = with pkgs; [
-            uv
             go
+            golangci-lint
+            gofumpt
           ];
         };
-      }
-    );
+
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+      };
+    };
 }
